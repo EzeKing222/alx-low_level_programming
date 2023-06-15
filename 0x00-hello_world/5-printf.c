1730fb30154b5a69adc22c5de5c97100a7c4e6ef
Eze Ikechukwu <ezeikechukwu007@gmail.com>
2023-06-15 15:04:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * outputin using printf
- */
+  * main - Entry point
+  * Return: always 0 (success)
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
