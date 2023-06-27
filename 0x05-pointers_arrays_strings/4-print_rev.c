395a099eb73e22487382b5e8ceb832145acb51c0
Eze Ikechukwu <ezeikechukwu007@gmail.com>
2023-06-26 18:26:46 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int begin = 0;
+
+	while (s[begin])
+		begin++;
+	while (begin--)
+		_putchar(s[begin]);
+	_putchar('\n');
+}
