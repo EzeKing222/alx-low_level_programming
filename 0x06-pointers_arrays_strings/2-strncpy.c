32a2e1f5610d6c4beb1918234f0572fe0389c3a6
Eze Ikechukwu <ezeikechukwu007@gmail.com>
2023-06-30 15:09:24 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strncpy - copies a string
+ *
+ * @dest: destination string pointer
+ *
+ * @src: source string pointer
+ *
+ * @n: number of bytes destination used
+ *
+ * Return: pointer to destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count;
+
+	for (count = 0; count < n && src[count] != '\0'; count++)
+		dest[count] = src[count];
+
+	for (; count < n; count++)
+		dest[count] = '\0';
+
+	return (dest);
+}
