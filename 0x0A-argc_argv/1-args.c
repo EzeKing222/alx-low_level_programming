9172797a46ffb265eb3db307c090255acd89924a
Eze Ikechukwu <ezeikechukwu007@gmail.com>
2023-07-12 20:29:18 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - entry point
- * @argc:
+ * @argc: number of entered command
+ * @argv: strings of commands entered
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc++);
+
+	return (0);
+
+}
