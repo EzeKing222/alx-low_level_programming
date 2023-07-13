6890f3fd9dd0d5ea51641016a06a2a535fc3d8b4
Eze Ikechukwu <ezeikechukwu007@gmail.com>
2023-07-12 20:15:22 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: ouput number of peremeters entered
+ * @argv: array of entered peremeters
+ * Return: 0 (sucess)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
