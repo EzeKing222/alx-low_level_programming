efe099c3a0a9d0a29606843816137d68d7e12cfe
EzeKing222 <ezeikechukwu007@gmail.com>
2023-10-03 17:27:10 +0100

    task two of 0x15 file i_0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd; _write;
-	len;
+	ssize_t fd; _write;
+	int len;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,11 +20,11 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	_write(fd, text_content, len);
+	_write = write(fd, text_content, len);
 
-	if (fd == -1 || _write == -1)
+	if ((fd == -1) || (_write == -1))
 		return (-1);
 
 	close(fd);
